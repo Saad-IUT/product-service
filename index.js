13f55d0c3ed4203d5035cf107a6889fe3ae99374
Saad-IUT <saadbinjohir@gmail.com>
2020-12-26 02:02:29 +0600

    Done

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// const { addProduct } = require('./products')
 const app = require('express')()
-// const jsonParser = require('body-parser').json()
-// // const cors = require('cors')()
+const jsonParser = require('body-parser').json()
+const cors = require('cors')()
 
-// app.use(jsonParser)
+app.use(cors, jsonParser)
 
 app.get('/', (req, res) => {
   res.send('OK')
 })
 
-// app.post('/product/add', addProduct)
-
-// let port = process.env.PORT || 3000
-// app.listen(port, () => {
-//   console.log(`Running on port ${port}`)
-// })
+let port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Running on port ${port}`)
+})
