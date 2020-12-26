857c0fd4c2268738f8ea867f5d0bc7d8ad970a01
Saad-IUT <saadbinjohir@gmail.com>
2020-12-26 13:26:43 +0600

    Done

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const app = require('express')()
 const jsonParser = require('body-parser').json()
 const cors = require('cors')()
 
-const { addProduct } = require('./products')
+const { addProduct, deleteProduct } = require('./products')
 
 app.use(cors, jsonParser)
 
+// Product routes
 app.post('/product/add', addProduct)
+// app.delete('/product/remove/:productId', deleteProduct)
 
 //Testing
 app.get('/', (req, res) => {
   res.send('OK')
 })
-
 let port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
--- products.js
+++ products.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require('uuid')
 const { db } = require('./admin')
+const { validateProduct } = require('./validators')
 
 exports.helloWorld = (request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true })
@@ -8,22 +9,49 @@ exports.helloWorld = (request, response) => {
 
 // Add Product
 exports.addProduct = (req, res) => {
-  const newProduct = {
-    name: req.body.name,
-    uuid: uuidv4(),
-  }
-  db.doc(`/products/${newProduct.name}`)
+  // const productRef = db.doc(`/products/${newProduct.name}`)
+  // const newProduct = {
+  //   name: req.body.name,
+  //   categoryId: req.body.categoryId,
+  // }
+  const valid = validateProduct(req.body.name)
+  console.log(valid)
+  // productRef
+  //   .get()
+  //   .then(doc => {
+  //     if (doc.exists) {
+  //       return res.status(400).json({ product: 'Duplicate' })
+  //     } else {
+  //       productRef.set(newProduct)
+  //       res.status(201).json({ message: 'Product successfully added' })
+  //     }
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({ error: 'something went wrong' })
+  //     console.error(err)
+  //   })
+}
+
+// Delete a product
+exports.deleteProduct = (req, res) => {
+  const productRef = db.doc(`/products/${req.params.productId}`)
+  productRef
     .get()
     .then(doc => {
-      if (doc.exists) {
-        return res.status(400).json({ product: 'Duplicate' })
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'product not found' })
+      }
+      if (doc.data().userHandle !== req.user.handle) {
+        return res.status(403).json({ error: 'Unauthorized' })
       } else {
-        db.doc(`/products/${newProduct.name}`).set(newProduct)
-        res.status(201).json({ message: 'Product successfully added' })
+        return productRef.delete()
       }
     })
+    .then(() => {
+      res.json({ message: 'product deleted successfully' })
+    })
     .catch(err => {
-      res.status(500).json({ error: 'something went wrong' })
       console.error(err)
+      return res.status(500).json({ error: err.code })
     })
 }
